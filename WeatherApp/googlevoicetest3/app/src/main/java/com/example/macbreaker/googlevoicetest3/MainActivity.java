5e705e5f16fef3b7cc1ad99f7500dabc94510ce8
jpeezzy <justidl3@uci.edu>
2017-05-31 11:55:55 -0700

    using activity instead of surfaceview

--- WeatherApp/googlevoicetest3/app/src/main/java/com/example/macbreaker/googlevoicetest3/MainActivity.java
+++ WeatherApp/googlevoicetest3/app/src/main/java/com/example/macbreaker/googlevoicetest3/MainActivity.java
@@ -1,24 +1,23 @@
 package com.example.macbreaker.googlevoicetest3;
-import java.lang.Thread;
+import java.util.Locale;
 
 import android.Manifest;
+import android.annotation.TargetApi;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.Activity;
-import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
-import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import java.util.Locale;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
-
+    ImageButton imageButton;
+    TextToSpeech t1;
+    EditText ed1;
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -26,11 +25,22 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         requestPermission();
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
-
-        setContentView(new BoardView(this));
+        setContentView(R.layout.activity_main);
+        imageButton = new ImageButton(this);
+        //   addListenerOnButton();
+        t1 = new TextToSpeech(this, new TextToSpeech.OnInitListener()
+        {
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR) {
+                    t1.setLanguage(Locale.UK);
+                }
+            }
+        } );
 
     }
-    private void requestPermission() {
+
+    private void requestPermission() { //allows me to access files
 
         if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
             Toast.makeText(MainActivity.this, "Write External Storage permission allows us to do store images. Please allow this permission in App Settings.", Toast.LENGTH_LONG).show();
@@ -38,4 +48,25 @@ public class MainActivity extends Activity {
             ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
         }
     }
+
+    public void buttonHit(View view) //whenh button is hit.
+    {
+        System.out.println("Gets to onClick");
+        try {
+            voiceTest(t1); //had an unhandled exception. Check later what it was, probably interrupted
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    protected void voiceTest(TextToSpeech t1) throws Exception {
+        ReadTextFile.readFile(); //unhandled exception
+        System.out.println("it gets to hello world?");
+        String temperature = ReadTextFile.readFile();
+        String toSpeak = "Hello Justin! Today is May 10. Todays weather will be" + temperature + " Degrees Farenheit." + " Wear" + ReadTextFile.whatToWear(temperature);
+        //t1.getVoice();
+        t1.speak(toSpeak, TextToSpeech.QUEUE_ADD, null, "id");
+    }
+
+
 }
---
+++ WeatherApp/googlevoicetest3/app/src/main/java/com/example/macbreaker/googlevoicetest3/ReadTextFile.java
@@ -0,0 +1,60 @@
+package com.example.macbreaker.googlevoicetest3;
+
+import android.os.Environment;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ * Created by Justin on 5/30/2017.
+ */
+
+public class ReadTextFile {
+    public static String readFile() throws FileNotFoundException //reads txt file and grabs temperature
+    {
+        //which returns the internal app files directory path
+        File sdcard = Environment.getExternalStorageDirectory();
+        File file = new File("/storage/8C04-1A48/DropboxSync/temperature.txt");
+        System.out.println("file directory is " + sdcard.toString());
+        //for galaxy s3 returns internal memory
+        StringBuilder text = new StringBuilder();
+        String line = "";
+        java.io.FileReader textFile = new java.io.FileReader(file);
+        try {
+            System.out.println("gets to first line of trycatch");
+            BufferedReader br = new BufferedReader(new java.io.FileReader(file));
+            String temp = br.readLine();
+            System.out.println("readline is " + temp);
+            line = temp;
+            //System.out.println("it goes into br statement. line = " + line);
+            text.append(line);
+            br.close();
+        } catch (IOException e) {
+            System.out.println("gets to exeception. try to avoid this");
+            //You'll need to add proper error handling here
+        }
+        System.out.println("text is " + text.toString());
+        return line;
+    }
+
+    public static String whatToWear(String Temperature){
+        int temp = Integer.parseInt(Temperature);
+        if(temp >=0 & temp < 60)
+            return "Heavy clothes, a beanie, shoes, and a thick windbreaker";
+        else if (temp>=60 & temp < 70)
+            return "A windbreaker and beanie";
+        else if(temp >= 70 & temp < 80)
+            return "Weather is perfect, go outside with a light jacket, sandals, and shorts ";
+        else if(temp >= 80 & temp >85 )
+            return "It's a little warm outside. Wear shorts, a loose tee-shirt, and sandals.";
+        else if(temp >= 85 & temp < 90 )
+            return "It's warm. Wear loose shorts and shirt";
+        else if(temp >= 90)
+            return "It's hot right now. Make sure to wear sunblock, as well as shorts and shirt.";
+        else
+            return "Soryr, i cannot recognize this temperature, please try again.";
+    }
+}
+
